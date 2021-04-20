d8308a3726ef599f84abe6542ae9e09d28aaed79
rayalva407 <rayalva407@gmail.com>
2021-04-20 16:49:56 -0600

    add method one

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
+
+    static void one() {
+        int i = 0;
+        String star = "*";
+        while (i <= 3) {
+            System.out.println(star);
+            star = star + "*";
+            i += 1;
+        }
+        System.out.println("----------");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        one();
     }
 }
