7dc427da19314c4bc5174bae8443e78e8475871e
rayalva407 <rayalva407@gmail.com>
2021-04-20 17:13:22 -0600

    adds two method

--- src/App.java
+++ src/App.java
@@ -2,16 +2,28 @@ public class App {
 
     static void one() {
         int i = 0;
-        String star = "*";
+        String str = "*";
         while (i <= 3) {
-            System.out.println(star);
-            star = star + "*";
+            System.out.println(str);
+            str = str + "*";
             i += 1;
         }
         System.out.println("----------");
     }
 
+    static void two() {
+        int i = 4;
+        String str = "****";
+        System.out.println("----------");
+        while (i > 0) {
+            System.out.println(str);
+            str = str.substring(0, str.length() - 1);
+            i -= 1;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         one();
+        two();
     }
 }
