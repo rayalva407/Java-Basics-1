4dfaa23c009125e5c13187d82513f02372c2f365
rayalva407 <rayalva407@gmail.com>
2021-04-21 23:00:40 -0600

    removes comments

--- src/App.java
+++ src/App.java
@@ -41,15 +41,6 @@ public class App {
         int num = 4;
         System.out.println("------------");
         for (int i = num; i >= 1; i--) {
-            // for (int j = i; j > 0; j--) {
-            //     System.out.print(" ");
-            // }
-
-            // for (int k = 1; k < (i * 2); k++) {
-            //     System.out.print("*");
-            // }
-
-            // System.out.println("");
             for (int j = num; j >= i; j--) {
                 System.out.print(" ");
             }
